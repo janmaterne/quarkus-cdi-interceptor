02ebf1bd44b5fc03aab62de2b1519fca78f677e5
Jan MatÃšrne <jan@materne.de>
2020-05-26 11:05:34 +0200

    change application root

--- src/test/java/de/materne/quarkus/ExampleResourceTest.java
+++ src/test/java/de/materne/quarkus/ExampleResourceTest.java
@@ -12,7 +12,7 @@ public class ExampleResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/")
           .then()
              .statusCode(200)
              .body(is("hello"));
